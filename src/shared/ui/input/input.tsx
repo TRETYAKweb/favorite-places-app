5a0363d4f6cf8055bcd9295652d95962bac4fa8d
TretyakWeb <tretyakbro@gmail.com>
2023-11-14 20:03:36 +0200

    feat(shared): add Input component

---
+++ src/shared/ui/input/input.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import {
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  TextInputProps,
+} from "react-native";
+import { FieldError, Noop } from "react-hook-form";
+import { colors, fonts } from "shared";
+
+type TypeMode = "light";
+
+interface InputProps {
+  label: string;
+  error?: FieldError | undefined;
+  inputProps?: TextInputProps;
+  mode?: TypeMode;
+  onBlur: Noop;
+  value: string;
+  onChange: (text: string) => void;
+  style?: { flex: number };
+}
+
+export const Input: React.FC<InputProps> = ({
+  label,
+  onBlur,
+  onChange,
+  value,
+  error,
+  inputProps,
+}) => {
+  const lableStyle = [styles.lable, error && styles.errorLable];
+  const inputStyle = [
+    styles.input,
+    inputProps?.multiline && styles.multiline,
+    error && styles.errorInput,
+  ];
+
+  return (
+    <View>
+      <Text style={lableStyle}>{label}</Text>
+      <TextInput
+        style={inputStyle}
+        onChangeText={onChange}
+        onBlur={onBlur}
+        value={value}
+        {...inputProps}
+      />
+      {error && <Text style={styles.textError}>{error.message}</Text>}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    marginHorizontal: 7.5,
+  },
+  lable: {
+    fontFamily: fonts.roboto700,
+    fontSize: 16,
+    color: colors.black,
+    marginBottom: 7,
+  },
+  errorLable: {
+    color: colors.error[500],
+  },
+  input: {
+    fontFamily: fonts.roboto400,
+    fontSize: 16,
+    borderWidth: 1,
+    borderColor: colors.white,
+    padding: 15,
+    borderRadius: 8,
+  },
+  errorInput: {
+    borderColor: colors.error[500],
+  },
+  textError: {
+    fontFamily: fonts.roboto400,
+    fontSize: 12,
+    color: colors.error[500],
+    marginTop: 5,
+  },
+  multiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+});
