4b5b0b4b33d1bb45bddecf1e9d1a6fdded81296b
TretyakWeb <tretyakbro@gmail.com>
2023-11-14 20:02:03 +0200

    feat(styles): add color constants to colors module

---
+++ src/shared/lib/styles/colors.ts
@@ -0,0 +1,23 @@
+export const colors = Object.freeze({
+  primary: {
+    200: "#EEEBFA",
+    300: "#ded4ff",
+    500: "#5D30F5",
+  },
+  //   accent: "#C8974B",
+  error: {
+    100: "#F0D4D5",
+    500: "#DB5147",
+  },
+  bg: {
+    100: "#F7F7FA",
+    300: "#EDECF6",
+  },
+  gray: {
+    300: "#ECECEF",
+    500: "#9f9f9f",
+    900: "#121617",
+  },
+  black: "#000",
+  white: "#fff",
+});
