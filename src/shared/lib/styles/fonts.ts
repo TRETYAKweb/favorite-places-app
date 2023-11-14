708dba0753d053a23c1e6b324c5e836352400796
TretyakWeb <tretyakbro@gmail.com>
2023-11-14 20:02:33 +0200

    feat(styles): add font assets and constants to fonts module

---
+++ src/shared/lib/styles/fonts.ts
@@ -0,0 +1,15 @@
+export const fontAssets = Object.freeze({
+  "roboto-300": require("../../assets/fonts/Roboto-Light.ttf"),
+  "roboto-400": require("../../assets/fonts/Roboto-Regular.ttf"),
+  "roboto-700": require("../../assets/fonts/Roboto-Bold.ttf"),
+  "gilroy-300": require("../../assets/fonts/Gilroy-Light.ttf"),
+  "gilroy-800": require("../../assets/fonts/Gilroy-ExtraBold.ttf"),
+});
+
+export const fonts = Object.freeze({
+  gilroy300: "gilroy-300",
+  gilroy800: "gilroy-800",
+  roboto300: "roboto-300",
+  roboto400: "roboto-400",
+  roboto700: "roboto-700",
+});
