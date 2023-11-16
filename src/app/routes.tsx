3f9c2a826142decd928cd85c3d903e9471bc7a3c
TretyakWeb <tretyakbro@gmail.com>
2023-11-16 21:03:03 +0200

    style(routes.tsx): enhance navigation styling and add header customization

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,14 +1,51 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
-import { AddPlaceScreen } from "screens";
+import { StyleSheet, Text } from "react-native";
+import { AddPlaceScreen, AllPlacesScreen } from "screens";
+import { colors, fonts } from "shared/lib";
+import { IconButton } from "shared/ui";
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigation: React.FC = () => {
+  const navigation = useNavigation();
+
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="AddPlace" component={AddPlaceScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerTransparent: true,
+        contentStyle: {
+          backgroundColor: colors.bg[300],
+        },
+      }}
+    >
+      <Stack.Screen
+        name="AllPlaces"
+        component={AllPlacesScreen}
+        options={{
+          headerTitle: () => <Text style={styles.headerTitle}>All Places</Text>,
+          headerRight: () => (
+            <IconButton
+              color={colors.primary[500]}
+              name="add-circle"
+              size={31}
+              onPress={() => navigation.navigate("AddPlace" as never)}
+            />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="AddPlace"
+        component={AddPlaceScreen}
+        options={{
+          headerBackTitleStyle: {
+            fontFamily: fonts.roboto700,
+          },
+          headerTitle: () => <Text style={styles.headerTitle}>Add Place</Text>,
+          headerTintColor: colors.primary[500],
+        }}
+      />
     </Stack.Navigator>
   );
 };
@@ -20,3 +57,10 @@ export const Routing: React.FC = () => {
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  headerTitle: {
+    fontFamily: fonts.gilroy800,
+    fontSize: 17,
+  },
+});
