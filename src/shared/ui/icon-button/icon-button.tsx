1fe0facb1348837fae21c02a7477e6726b6e9f21
TretyakWeb <tretyakbro@gmail.com>
2023-11-21 23:40:56 +0200

    chore(shared-ui): add 'bookmark' icon to IconButton

--- src/shared/ui/icon-button/icon-button.tsx
+++ src/shared/ui/icon-button/icon-button.tsx
@@ -2,7 +2,7 @@ import { Ionicons } from "@expo/vector-icons";
 import React from "react";
 import { Pressable, StyleSheet } from "react-native";
 
-type IconName = "add" | "add-circle";
+type IconName = "add" | "add-circle" | "bookmark";
 
 interface IconButtonProps {
   name: IconName;
