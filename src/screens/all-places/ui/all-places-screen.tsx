82dcd83fe1efb46b86a9fc659778d8435e3a7b3c
TretyakWeb <tretyakbro@gmail.com>
2023-11-25 19:13:49 +0200

    feat(all-places): display newly added place in the list
    - Use the 'useEffect' hook to update the list of loaded places when a new place is added.
    - Pass the updated list of places to the 'PlacesList' component.

--- src/screens/all-places/ui/all-places-screen.tsx
+++ src/screens/all-places/ui/all-places-screen.tsx
@@ -1,13 +1,37 @@
 import { StyleSheet, View } from "react-native";
 import { PlacesList } from "widgets";
 import { useHeaderHeight } from "@react-navigation/elements";
+import { RouteProp, useIsFocused, useRoute } from "@react-navigation/native";
+import { useEffect, useState } from "react";
+import { IPlace } from "shared/models";
+
+type Param = {
+  AllPlaces: {
+    newPlace: {
+      title: string;
+      imgUri: string;
+      address: string;
+      location: { lat: number; lng: number };
+      id: string;
+    };
+  };
+};
 
 export const Screen = () => {
+  const [loadedPlaces, setLoadedPlaces] = useState<IPlace[] | []>([]);
+  const isFocused = useIsFocused();
+  const route = useRoute<RouteProp<Param>>();
   const headerHeight = useHeaderHeight();
+
+  useEffect(() => {
+    if (route.params) {
+      setLoadedPlaces((prev) => [...prev, route.params.newPlace]);
+    }
+  }, [route.params, isFocused]);
 
   return (
     <View style={[styles.root, { marginTop: headerHeight }]}>
-      <PlacesList places={[]} />
+      <PlacesList places={loadedPlaces} />
     </View>
   );
 };
@@ -15,8 +39,6 @@ export const Screen = () => {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     padding: 25,
   },
 });
