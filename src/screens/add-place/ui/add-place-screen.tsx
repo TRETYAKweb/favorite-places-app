d931e70c055c8b54eeb5c4db79b27758c0a85d1d
TretyakWeb <tretyakbro@gmail.com>
2023-11-25 19:13:25 +0200

    feat(add-place): integrate location and image data in place creation
    - Ensure that a photo is selected before submitting.
    - Require the selection of a location before submitting.
    - Create a new 'Place' instance with the provided data and navigate to 'AllPlaces' screen with the new place.

--- src/screens/add-place/ui/add-place-screen.tsx
+++ src/screens/add-place/ui/add-place-screen.tsx
@@ -4,25 +4,49 @@ import { StyleSheet, View } from "react-native";
 import { useState } from "react";
 import { ImagePicker, LocationPicker } from "features";
 import { IFormData } from "features/place/add-place/model";
-import { api } from "shared/api";
+import { useNavigation } from "@react-navigation/native";
+import { openNotificationError } from "shared/ui";
+import { Place } from "shared/models";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { ScreenName } from "shared/lib";
+
+type Param = {
+  AllPlaces: {
+    newPlace: {
+      title: string;
+      imgUri: string;
+      address: string;
+      location: { lat: number; lng: number };
+      id: string;
+    };
+  };
+};
 
 export const Screen = () => {
   const [pickedImage, setPickedImage] = useState<string>("");
   const [pickedLocation, setPickedLocation] = useState<{
     latitude: number;
     longitude: number;
+    address: string;
   } | null>(null);
-  const [address, setAddress] = useState<string | null>(null);
   const headerHeight = useHeaderHeight();
+  const navigation = useNavigation<StackNavigationProp<Param>>();
 
   const onSubmit = async (data: IFormData) => {
-    if (pickedLocation) {
-      const address = await api.getAdress(
-        pickedLocation?.latitude,
-        pickedLocation?.longitude
-      );
-      typeof address === "string" && setAddress(address);
+    if (!pickedImage) {
+      openNotificationError("Нужна фотка");
+      return;
     }
+    if (!pickedLocation) {
+      openNotificationError("Нужно гео");
+      return;
+    }
+    const place = new Place(data.title, pickedImage, pickedLocation.address, {
+      lat: pickedLocation.latitude,
+      lng: pickedLocation.longitude,
+    });
+    console.log(place);
+    navigation.navigate(ScreenName.AllPlaces, { newPlace: place });
   };
 
   return (
