5a346096b8f74d4e44c51655a1fcd57d066d536f
TretyakWeb <tretyakbro@gmail.com>
2023-11-25 19:10:14 +0200

    style(place-card): improve styling and add press effect
    - Styled the PlaceCard component to improve its appearance.
    - Added a press effect to enhance user interaction.

--- src/entities/place/ui/place-card.tsx
+++ src/entities/place/ui/place-card.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Image, Pressable, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { colors, fonts } from "shared/lib";
 
 interface ICardProps {
   title: string;
@@ -9,12 +10,46 @@ interface ICardProps {
 
 export const Card: React.FC<ICardProps> = ({ title, address, imgUri }) => {
   return (
-    <Pressable>
-      <Image source={{ uri: imgUri }} />
-      <View>
-        <Text>{title}</Text>
-        <Text>{address}</Text>
+    <Pressable
+      style={({ pressed }) => [styles.root, pressed && styles.pressed]}
+    >
+      <Image style={styles.image} source={{ uri: imgUri }} />
+      <View style={styles.inner}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.address}>{address}</Text>
       </View>
     </Pressable>
   );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    borderRadius: 8,
+    backgroundColor: colors.white,
+  },
+  image: {
+    flex: 1,
+    borderTopLeftRadius: 8,
+    borderBottomLeftRadius: 8,
+    height: 100,
+  },
+  inner: {
+    flex: 2,
+    padding: 12,
+  },
+  title: {
+    fontFamily: fonts.gilroy800,
+    fontSize: 16,
+    marginBottom: 7,
+  },
+  address: {
+    fontFamily: fonts.roboto400,
+    fontSize: 14,
+    lineHeight: 17,
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+});
