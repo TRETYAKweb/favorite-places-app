3dcb6604fb4c1cbbf1a31cd568ae183a0a5dcb66
TretyakWeb <tretyakbro@gmail.com>
2023-11-26 05:19:33 +0200

    fix(models): add 'id' parameter to the Place constructor
    - Updated the Place constructor in the models to include the 'id' parameter, ensuring proper initialization.

--- src/shared/models/place.ts
+++ src/shared/models/place.ts
@@ -16,12 +16,13 @@ export class Place {
     title: string,
     imgUri: string,
     address: string,
-    location: { lat: number; lng: number }
+    location: { lat: number; lng: number },
+    id: string
   ) {
     this.title = title;
     this.imgUri = imgUri;
     this.address = address;
     this.location = location;
-    this.id = new Date().toString() + Math.random().toString();
+    this.id = id;
   }
 }
