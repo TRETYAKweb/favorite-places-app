ad378a8b409bc3c0dda2de1efcf872f3d8315e1a
TretyakWeb <tretyakbro@gmail.com>
2023-11-26 05:18:29 +0200

    feat(database): implement database functions for inserting, fetching all places, and fetching place details
    - Added functions to insert a place into the database, fetch all places, and fetch place details by ID.
    - Created an interface `IPlaceSQLite` to represent the structure of places stored in the SQLite database.

--- src/shared/lib/database-sqlite/database.ts
+++ src/shared/lib/database-sqlite/database.ts
@@ -1,4 +1,14 @@
 import { openDatabase, SQLError, SQLTransaction } from "expo-sqlite";
+import { IPlace, Place } from "shared/models";
+
+export interface IPlaceSQLite {
+  id: number;
+  title: string;
+  imageUri: string;
+  address: string;
+  lat: number;
+  lng: number;
+}
 
 const database = openDatabase("places.db");
 
@@ -19,6 +29,86 @@ export const init = () => {
           resolve();
         },
         (_: SQLTransaction, error: SQLError) => {
+          reject(error);
+          return true;
+        }
+      );
+    });
+  });
+
+  return promise;
+};
+
+export const insertPlace = (place: IPlace) => {
+  const promise = new Promise((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `INSERT INTO places (title, imageUri, address, lat, lng) VALUES (?, ?, ?, ?, ?)`,
+        [
+          place.title,
+          place.imgUri,
+          place.address,
+          place.location.lat,
+          place.location.lng,
+        ],
+        (_, result) => {
+          resolve(result);
+        },
+        (_, error) => {
+          reject(error);
+          return true;
+        }
+      );
+    });
+  });
+
+  return promise;
+};
+
+export const fetchPlaces = () => {
+  const promise = new Promise<IPlace[]>((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        `SELECT * FROM places`,
+        [],
+        (_, result) => {
+          const places: IPlace[] = [];
+
+          for (const db of result.rows._array as IPlaceSQLite[]) {
+            places.push(
+              new Place(
+                db.title,
+                db.imageUri,
+                db.address,
+                { lat: db.lat, lng: db.lng },
+                db.id.toString()
+              )
+            );
+          }
+
+          resolve(places);
+        },
+        (_, error) => {
+          reject(error);
+          return true;
+        }
+      );
+    });
+  });
+
+  return promise;
+};
+
+export const fetchPlaceDetails = (id: string) => {
+  const promise = new Promise<IPlaceSQLite>((resolve, reject) => {
+    database.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM places WHERE id = ?",
+        [id],
+        (_, result) => {
+          resolve(result.rows._array[0]);
+        },
+        (_, error) => {
           reject(error);
           return true;
         }
