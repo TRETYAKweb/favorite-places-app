5e0fa0adaedc63e4663e56b3ab5d9a1045adc088
TretyakWeb <tretyakbro@gmail.com>
2023-11-26 05:19:54 +0200

    feat(places-list): navigate to PlaceDetails on item press
    - Added navigation to the PlaceDetails screen when an item is pressed in the PlacesList component.

--- src/widgets/places-list/ui/places-list.tsx
+++ src/widgets/places-list/ui/places-list.tsx
@@ -1,14 +1,28 @@
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
 import { PlaceCard } from "entities";
 import React from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
-import { fonts } from "shared/lib";
+import { ScreenName, fonts } from "shared/lib";
 import { IPlace } from "shared/models";
 
 interface IListProps {
   places: IPlace[];
 }
 
+type Param = {
+  PlaceDetails: {
+    id: string;
+  };
+};
+
 export const List: React.FC<IListProps> = ({ places }) => {
+  const navigation = useNavigation<StackNavigationProp<Param>>();
+
+  const pressHandler = (id: string) => {
+    navigation.navigate(ScreenName.PlaceDetails, { id });
+  };
+
   if (!places || places.length === 0) {
     return (
       <View style={styles.fallbackContainer}>
@@ -24,6 +38,7 @@ export const List: React.FC<IListProps> = ({ places }) => {
       keyExtractor={(item) => item?.id}
       renderItem={({ item }) => (
         <PlaceCard
+          onPress={() => pressHandler(item.id)}
           title={item.title}
           address={item.address}
           imgUri={item.imgUri}
