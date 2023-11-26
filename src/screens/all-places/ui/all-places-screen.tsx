585c66dc2545d431e0c4135c08f946d1b5bd345d
TretyakWeb <tretyakbro@gmail.com>
2023-11-26 05:17:42 +0200

    chore(all-places): use fetchPlaces function on screen focus
    - Replaced the manual addition of a new place with a call to the fetchPlaces function when the AllPlaces screen is focused.

--- src/screens/all-places/ui/all-places-screen.tsx
+++ src/screens/all-places/ui/all-places-screen.tsx
@@ -1,33 +1,24 @@
 import { StyleSheet, View } from "react-native";
 import { PlacesList } from "widgets";
 import { useHeaderHeight } from "@react-navigation/elements";
-import { RouteProp, useIsFocused, useRoute } from "@react-navigation/native";
+import { useIsFocused } from "@react-navigation/native";
 import { useEffect, useState } from "react";
 import { IPlace } from "shared/models";
-
-type Param = {
-  AllPlaces: {
-    newPlace: {
-      title: string;
-      imgUri: string;
-      address: string;
-      location: { lat: number; lng: number };
-      id: string;
-    };
-  };
-};
+import { fetchPlaces } from "shared/lib";
 
 export const Screen = () => {
   const [loadedPlaces, setLoadedPlaces] = useState<IPlace[] | []>([]);
   const isFocused = useIsFocused();
-  const route = useRoute<RouteProp<Param>>();
   const headerHeight = useHeaderHeight();
 
   useEffect(() => {
-    if (route.params) {
-      setLoadedPlaces((prev) => [...prev, route.params.newPlace]);
+    if (isFocused) {
+      (async () => {
+        const places = await fetchPlaces();
+        setLoadedPlaces(places);
+      })();
     }
-  }, [route.params, isFocused]);
+  }, [isFocused]);
 
   return (
     <View style={[styles.root, { marginTop: headerHeight }]}>
